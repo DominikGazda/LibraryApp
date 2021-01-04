64aef447831d7a9391f4abb1e9553d7ba60a6c9b
Dominik <dominikgzd@gmail.com>
2021-01-04 10:47:50 +0100

    Add AuthorResource Unit Tests

--- src/test/java/pl/library/components/author/AuthorResourceTest.java
+++ src/test/java/pl/library/components/author/AuthorResourceTest.java
@@ -1,13 +1,11 @@
 package pl.library.components.author;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
@@ -15,17 +13,13 @@ import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.server.ResponseStatusException;
-import pl.library.components.address.Address;
+import pl.library.components.book.BookDto;
 
-import java.awt.*;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -209,12 +203,68 @@ class AuthorResourceTest {
         }
     }
 
+    @DisplayName("DeleteAuthorTest")
+    @Test
+    void deleteAuthorTest() throws Exception {
+        //given
+        AuthorDto authorToDelete = createAuthor(1L,"Bartosz","Kozak");
+        String url = "/api/author/{id}";
+        //when
+        when(authorService.deleteAuthor(anyLong())).thenReturn(authorToDelete);
+        //then
+        mockMvc.perform(delete(url, anyLong())
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.authorId").exists())
+                .andExpect(jsonPath("$.authorName").value("Bartosz"))
+                .andExpect(jsonPath("$.authorSurname").value("Kozak"))
+                .andExpect(status().isOk());
+    }
+
+    @DisplayName("GetAllBooksForAuthorByIdTest")
+    @Test
+    void getAllBooksForAuthorByIdTest() throws Exception {
+        //given
+        BookDto firstBook = createBookDto(1L,"Lew","1234567890987",10,"WSIP");
+        BookDto secondBook = createBookDto(2L,"Bajka","1234567890900",20,"Nowa Era");
+        List<BookDto> bookList = List.of(firstBook, secondBook);
+        String url = "/api/author/{id}/books";
+        //when
+        when(authorService.getAllBooksForAuthorById(anyLong())).thenReturn(bookList);
+        //then
+        mockMvc.perform(get(url, anyLong())
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$[0].bookid").value(1L))
+                .andExpect(jsonPath("$[0].bookName").value("Lew"))
+                .andExpect(jsonPath("$[0].isbn").value("1234567890987"))
+                .andExpect(jsonPath("$[0].availableQuantity").value(10))
+                .andExpect(jsonPath("$[0].publisher").value("WSIP"))
+                .andExpect(jsonPath("$[1].bookid").value(2L))
+                .andExpect(jsonPath("$[1].bookName").value("Bajka"))
+                .andExpect(jsonPath("$[1].isbn").value("1234567890900"))
+                .andExpect(jsonPath("$[1].availableQuantity").value(20))
+                .andExpect(jsonPath("$[1].publisher").value("Nowa Era"))
+                .andExpect(status().isOk());
+
+
+    }
+
     private AuthorDto createAuthor(Long id, String name, String surname){
         AuthorDto author = new AuthorDto();
         author.setAuthorId(id);
         author.setAuthorName(name);
         author.setAuthorSurname(surname);
         return author;
+    }
+
+    private BookDto createBookDto(Long id, String name, String isbn, int quantity, String publisher){
+        BookDto bookDto = new BookDto();
+
+        bookDto.setBookid(id);
+        bookDto.setBookName(name);
+        bookDto.setAvailableQuantity(quantity);
+        bookDto.setIsbn(isbn);
+        bookDto.setPublisher(publisher);
+        return bookDto;
     }
 
 
